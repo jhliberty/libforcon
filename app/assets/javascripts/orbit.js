717b28900f26e3ad280d71c66ffc9347fa7aba7c
John-Henry Liberty <johnhenry.liberty@gmail.com>
2012-02-02 16:29:54 -0600

    initial commit with basic css

---
+++ app/assets/javascripts/orbit.js
@@ -0,0 +1,14 @@
+/* PLACEHOLDER FOR FORMS ------------- */
+/* Remove this and jquery.placeholder.min.js if you don't need :) */
+
+$('input, textarea').placeholder();
+
+
+// Yes
+     $(window).load(function() {
+         $('#featured').orbit({
+           timer: true,
+           captions: true, 	
+           bullets: true
+         });
+     });
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Liberty::Application.config.secret_token = '3a912591be4ad9cdf4d120b4d04539860e6a0a51c6b8cbb1c18cf1a1cf37a79efb378193794d9c3b03d74633ce03c024a5d5eb66dd90d19f286c8161bbe72fc3'
