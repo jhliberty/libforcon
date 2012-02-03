805e23ab9dbd6eadddf3048a62c016ade7ae1e96
John-Henry Liberty <johnhenry.liberty@gmail.com>
2012-02-03 17:25:19 -0600

    trying to split scopes of routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,5 +6,30 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find_by_permalink(params[:id])
+    unless @post
+      redirect_to posts_path
+    end
+  end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(params[:id])
+    if @post.save
+      flash[:notice] = "Success"
+      redirect_to(post_index_path)
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
   end  
 end
