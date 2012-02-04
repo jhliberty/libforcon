584e3d2fd0af2d56a2629d34387e9562a6eca48c
John-Henry Liberty <johnhenry.liberty@gmail.com>
2012-02-04 01:37:54 -0600

    added attr accessible and commented mailer out for local testing

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,10 +8,10 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     if @contact.valid?
       @contact.save!
-      ContactMailer.contact_us(@contact).deliver!
+      # ContactMailer.contact_us(@contact).deliver!
       redirect_to root_path, :notice => "We have received your email and will be in touch soon."
     else
-      render :contact
+      render :new
     end
   end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,7 @@
 class Contact < ActiveRecord::Base
 
-  validates_presence_of :email, :name, :content
+  validates_presence_of :email, :name, :body
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
   
-  attr_accessor :email, :name, :content
+  attr_accessible :email, :name, :subject, :body
 end
