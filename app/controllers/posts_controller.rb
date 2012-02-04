f452132a1f8463fd016331a6dd355fb5f68349db
John-Henry Liberty <johnhenry.liberty@gmail.com>
2012-02-03 23:57:18 -0600

    update and create posts works

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,20 +16,26 @@ class PostsController < ApplicationController
   end
   
   def create
-    @post = Post.new(params[:id])
+    @post = Post.new(params[:post])
     if @post.save
       flash[:notice] = "Success"
-      redirect_to(post_index_path)
+      redirect_to(post_path(@post.permalink))
     else
       render 'new'
     end
   end
   
   def edit
-    
+    @post = Post.find_by_permalink(params[:id])
   end
   
   def update
-    
+    @post = Post.find_by_permalink(params[:id])
+    if @post.update_attributes(params[:post])
+      flash[:notice] = "Success"
+      redirect_to(post_path(@post.permalink))
+    else
+      render 'edit'
+    end
   end  
 end
